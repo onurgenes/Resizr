2432135fa9bcb75eee2a4888b31bb5236ae8ea25
Onur Genes <onur@onurgenes.com>
2019-03-05 20:49:12 +0300

    Initial Commit

---
+++ Resizr/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Resizr
+//
+//  Created by Onur Geneş on 5.03.2019.
+//  Copyright © 2019 Onur Geneş. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Resizr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Resizr
+//
+//  Created by Onur Geneş on 5.03.2019.
+//  Copyright © 2019 Onur Geneş. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
